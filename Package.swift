85dd751d59d314d88a03a83cc0ee970c7891d0fb
Makan Taghizadeh <me@mkntz.com>
2025-09-18 22:59:04 +0200

    feat(di): implement basics of dependency injection

--- Package.swift
+++ Package.swift
@@ -4,23 +4,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "UtilityKit",
+    name: "swift-utility-kit",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "UtilityKit",
-            targets: ["UtilityKit"]
+            name: "DI",
+            targets: ["DI"]
         )
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "UtilityKit"
-        ),
-        .testTarget(
-            name: "UtilityKitTests",
-            dependencies: ["UtilityKit"]
-        ),
+        .target(name: "DI"),
     ]
 )
